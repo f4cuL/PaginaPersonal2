0ef028fdcbe1c7d0b06a67f2dffab1057d54011d
f4cuL <facueliaslopez@gmail.com>
2021-01-09 16:27:49 -0300

    espacios

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var app = new Vue({
     message: 'Hola mundo!',
     mostrar: true,
     texto1: "Me encuentro realizando la carrera de Sistemas, por el momento manejo HTML, CSS, algunos frameworks de CSS, Python, Javascript y Java, me encanta el desarrollo de software y la implementación de los mismos en diferentes entornos. Interesado en nuevos lenguajes y el desarrollo de aplicaciones de escritorio. Siempre con buena actitud ante los desafíos y la rápida búsqueda de soluciones. Siempre buscando nuevos aprendizajes y retos para mejorar mis capacidades como desarrollador e individuo. Me puedo desenvolver sin problemas con el trabajo en equipo, ya qué es una buena fuente de retroalimentación.",
-    info: ' Información',
+    info: 'Información',
     infoTexto: 'Actualmente no poseo experiencia laboral. Soy una persona muy calma y tranquila, ideal para la atención al cliente o la resolución de problemas que se plantean al momento. Poseo un buen manejo de responsabilidad a la hora de ejercer, tengo conocimientos avanzados sobre tecnología en general y un excelente uso de computadoras y algún que otro programa, ya sea Word, Excel, entre otras. Puedo desempeñarme en la programación, ya sea web o apps, con diferentes lenguajes y frameworks.',
     estudios: ' Estudios',
     nere: 'Instituto Nere echea (2003-2017)',
@@ -103,7 +103,7 @@ btn.addEventListener( 'change', function() {
 
   document.querySelector('.containerCV').classList.replace("animate__fadeIn","animate__fadeOut");
 
-  setTimeout(function(){app.info=' Información'},900);
+  setTimeout(function(){app.info='Información'},900);
   setTimeout(function(){app.infoTexto='Actualmente no poseo experiencia laboral. Soy una persona muy calma y tranquila, ideal para la atención al cliente o la resolución de problemas que se plantean al momento. Poseo un buen manejo de responsabilidad a la hora de ejercer, tengo conocimientos avanzados sobre tecnología en general y un excelente uso de computadoras y algún que otro programa, ya sea Word, Excel, entre otras. Puedo desempeñarme en la programación, ya sea web o apps, con diferentes lenguajes y frameworks.'},900);
   setTimeout(function(){app.estudios=' Estudios'},900);
   setTimeout(function(){app.nereInfo='Secundaria y Primaria con orientación a ciéncias sociales.'},900);
