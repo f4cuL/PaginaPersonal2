cf9892eaaa5ad597607edbac7c01fa97e7fee972
Facundo Lopez <56969887+f4cuL@users.noreply.github.com>
2021-08-19 07:15:12 -0300

    fix

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var app = new Vue({
     mostrar: true,
     texto1: "Me encuentro realizando la carrera de Sistemas, por el momento manejo HTML, CSS, algunos frameworks de CSS, Python, Javascript y Java, me encanta el desarrollo de software y la implementación de los mismos en diferentes entornos. Interesado en nuevos lenguajes y el desarrollo de aplicaciones de escritorio. Siempre con buena actitud ante los desafíos y la rápida búsqueda de soluciones. Siempre buscando nuevos aprendizajes y retos para mejorar mis capacidades como desarrollador e individuo. Me puedo desenvolver sin problemas con el trabajo en equipo, ya qué es una buena fuente de retroalimentación.",
     info: 'Información',
-    infoTexto: 'Actualmente no poseo experiencia laboral. Soy una persona muy calma y tranquila, ideal para la atención al cliente o la resolución de problemas que se plantean al momento. Poseo un buen manejo de responsabilidad a la hora de ejercer, tengo conocimientos avanzados sobre tecnología en general y un excelente uso de computadoras y algún que otro programa, ya sea Word, Excel, entre otras. Puedo desempeñarme en la programación, ya sea web o apps, con diferentes lenguajes y frameworks.',
+    infoTexto: 'Soy una persona muy calma y tranquila, ideal para la atención al cliente o la resolución de problemas que se plantean al momento. Poseo un buen manejo de responsabilidad a la hora de ejercer, tengo conocimientos avanzados sobre tecnología en general y un excelente uso de computadoras y algún que otro programa, ya sea Word, Excel, entre otras. Puedo desempeñarme en la programación, ya sea web o apps, con diferentes lenguajes y frameworks.',
     estudios: ' Estudios',
     nere: 'Instituto Nere echea (2003-2017)',
     nereInfo: 'Secundaria y Primaria con orientación a ciéncias sociales.',
@@ -64,7 +64,7 @@ btn.addEventListener( 'change', function() {
   document.querySelector('.containerCV').classList.replace("animate__fadeIn","animate__fadeOut");
 
   setTimeout(function(){app.info=' Information'},900);
-  setTimeout(function(){app.infoTexto='Nowadays i don’t have working experience. I am fit to work as a customer supporter since i am a really calm person. I have great knowledge about technology and a great usage of computers and softwares like Excel, Word, etc. I can perform good in web and apps programming, using different languages and frameworks.'},900);
+  setTimeout(function(){app.infoTexto='I am fit to work as a customer supporter since i am a really calm person. I have great knowledge about technology and a great usage of computers and softwares like Excel, Word, etc. I can perform good in web and apps programming, using different languages and frameworks.'},900);
   setTimeout(function(){app.estudios=' Education'},900);
   setTimeout(function(){app.nereInfo='Primary and Secondary school.'},900);
   setTimeout(function(){app.unla='Universidad Nacional de Lanús (2019-present)'},900);
